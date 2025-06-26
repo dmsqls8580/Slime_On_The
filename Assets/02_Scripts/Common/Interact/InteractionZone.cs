77fdcf2f8d21e2cc3b1d8d625fc62de289897621
wnstlrWkd123 <prkjnsk@naver.com>
2025-06-26 19:46:53 +0900

    feat: InteractionZone.cs 추가
    플레이어 주위에서 상호작용 가능한 오브젝트를 찾아 HashSet에 넣어줌.

---
+++ Assets/02_Scripts/Common/Interact/InteractionZone.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionZone : MonoBehaviour
+{
+    // NPC, Building, 자원들(Mineral, Tree, Flower, Crop) || 통합으로 Resource
+    [SerializeField] private LayerMask interactableMask;
+
+    private HashSet<Collider2D> interactables = new HashSet<Collider2D>();
+    public HashSet<Collider2D> Interactables => interactables;
+
+    private bool IsInteractable(Collider2D collision)
+    {
+        return (interactableMask & (1 << collision.gameObject.layer)) != 0;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (IsInteractable(collision) && !interactables.Contains(collision))
+        {
+            interactables.Add(collision);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (interactables.Contains(collision))
+        {
+            interactables.Remove(collision);
+        }
+    }
+}
