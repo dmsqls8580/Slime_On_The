d1196436f0870c7a032202d2d9f4f5ee95dc8db4
KimgiChan07 <wekorean0727@naver.com>
2025-06-30 11:44:17 +0900

    feat: 애니메이션 제작, 구현

---
+++ Assets/02_Scripts/Player/PlayerAnimationController.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class PlayerAnimationController : MonoBehaviour
+{
+    [SerializeField] private PlayerAnimationDataSO animationDataSo;
+    public PlayerAnimationDataSO AnimationDataSo => animationDataSo;
+
+    public Animator Animator { get; private set; }
+
+    private void Awake()
+    {
+        Animator = GetComponent<Animator>();
+    }
+
+    public void SetMove(bool _isMoving)
+    {
+        Animator.SetBool(AnimationDataSo.isMoveParameter, _isMoving);
+    }
+
+    public void TriggerAttack()
+    {
+        Animator.SetTrigger(AnimationDataSo.IsAttackTriggerHash);
+    }
+
+    public void SetLook(Vector2 _lookDir)
+    {
+        Animator.SetFloat(AnimationDataSo.MouseXParameterHash,Mathf.Abs(_lookDir.x));
+        Animator.SetFloat(AnimationDataSo.MouseYParameterHash,_lookDir.y);
+    }
+}
