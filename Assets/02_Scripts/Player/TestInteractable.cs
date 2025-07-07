2fba8210d9dca6c13233cb79a6749f95236bdf3a
KimgiChan07 <wekorean0727@naver.com>
2025-07-07 10:19:32 +0900

    docs: 불필요, 어색한 파일정리

--- Assets/02_Scripts/Player/TestInteractable.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class TestInteractable : MonoBehaviour, IInteractable
-{
-    public void Interact(InteractionCommandType type)
-    {
-        Debug.Log($"Interacting with {type}");
-    }
-}
