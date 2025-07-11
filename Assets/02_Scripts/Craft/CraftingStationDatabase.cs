e0071a4077e493725986e2489028122e13411399
wnstlrWkd123 <prkjnsk@naver.com>
2025-07-11 19:49:49 +0900

    feat : normal아이템 idx 추가
    database에 아이템 idx를 전부 normal에 추가.

--- Assets/02_Scripts/Craft/CraftingStationDatabase.cs
+++ Assets/02_Scripts/Craft/CraftingStationDatabase.cs
@@ -8,7 +8,10 @@ public class CraftingStationDatabase : MonoBehaviour
     // 기본 제작 가능한 아이템 idx.
     private int[] normal = new int[]
     {
-        // 예) 101, 204, 505...
+        0, 2, 101, 102, 103, 201, 202, 203, 301, 302,
+        303, 401, 402, 403, 404, 411, 412, 413, 414, 416,
+        421, 422, 423, 424, 425, 426, 431, 432, 433, 434,
+        435, 436, 501, 502, 601, 602, 701, 801, 802
     };
     // 제작대로 제작 가능한 아이템 idx.
     private int[] workbench = new int[]
