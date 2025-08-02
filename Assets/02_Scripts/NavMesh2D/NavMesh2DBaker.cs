bc66768a41f527dbfb7c32d64f8d96b014622fb7
mak2u-park <scorio1234@gmail.com>
2025-08-03 04:00:44 +0900

    feat : ModifierObject

--- Assets/02_Scripts/NavMesh2D/NavMesh2DBaker.cs
+++ Assets/02_Scripts/NavMesh2D/NavMesh2DBaker.cs
@@ -7,10 +7,10 @@ using UnityEngine.AI;
 public class NavMesh2DManager : SceneOnlySingleton<NavMesh2DManager>
 {
     public NavMeshSurface Surface2D;
-    [SerializeField] private float tickInterval;
-    
-    private void Start()
+
+    protected override void Awake()
     {
+        base.Awake();
         Surface2D = GetComponent<NavMeshSurface>();
     }
 
@@ -26,16 +26,8 @@ public class NavMesh2DManager : SceneOnlySingleton<NavMesh2DManager>
         yield return null;
         Logger.Log("Baking NavMesh");
         Surface2D.BuildNavMesh();
-        // StartCoroutine(TickUpdate());
     }
-    private IEnumerator TickUpdate()
-    {
-        while (true)
-        {
-            UpdateThisNavMesh();
-            yield return new WaitForSeconds(tickInterval);
-        }
-    }
+
     
     // 오브젝트(자원, 건물, 등) 스폰, 설치, 파괴, 틱(30초) 당 호출
     public void UpdateThisNavMesh()
