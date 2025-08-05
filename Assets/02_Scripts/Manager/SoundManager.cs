2669b284649a27c9a093d6b46a026bd30ea400c8
Ragura635 <ragura635@gmail.com>
2025-08-06 02:15:52 +0900

    feat: 사운드매니저 환경음 추가, UI연출 개선

--- Assets/02_Scripts/Manager/SoundManager.cs
+++ Assets/02_Scripts/Manager/SoundManager.cs
@@ -1,7 +1,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
 public enum BGM
 {
     WeatherRainSound,
@@ -27,6 +26,11 @@ public enum SFX
     ItemPickup,
 }
 
+public enum Ambient
+{
+    
+}
+
 public class SoundManager : Singleton<SoundManager>
 {
     [Header("BGM")]
@@ -36,11 +40,16 @@ public class SoundManager : Singleton<SoundManager>
     [Header("SFX")]
     [SerializeField] private AudioSource SFXSource;
     [SerializeField] private List<AudioClip> SFXClips;
+    
+    [Header("AmbientSource")]
+    [SerializeField] private AudioSource AmbientSource;
+    [SerializeField] private List<AudioClip> AmbientClips;
 
     private BGM? currentBGM = null;
 
     public float GetBGMVolume() => BGMSource.volume;
     public float GetSFXVolume() => SFXSource.volume;
+    public float GetAmbientVolume() => AmbientSource.volume;
 
     protected override void Awake()
     {
@@ -73,6 +82,14 @@ public class SoundManager : Singleton<SoundManager>
         float volumeScale = GetSFXVolumeScale(sfx);
         SFXSource.PlayOneShot(SFXClips[(int)sfx], volumeScale);
     }
+    
+    public void PlayAmbient(Ambient ambient)
+    {
+        AudioClip clip = BGMClips[(int)ambient];
+        BGMSource.clip = clip;
+        BGMSource.loop = true;
+        BGMSource.Play();
+    }
 
     public void ChangeBGMVolume(float volume)
     {
@@ -82,6 +99,11 @@ public class SoundManager : Singleton<SoundManager>
     public void ChangeSFXVolume(float volume)
     {
         SFXSource.volume = volume;
+    }
+    
+    public void ChangeAmbientVolume(float volume)
+    {
+        AmbientSource.volume = volume;
     }
     
     private float GetSFXVolumeScale(SFX sfx)
--- Assets/02_Scripts/UI/Setting/UISoundSettings.cs
+++ Assets/02_Scripts/UI/Setting/UISoundSettings.cs
@@ -5,14 +5,26 @@ public class UISoundSettings : MonoBehaviour
 {
     [SerializeField] private Slider bgmSlider;
     [SerializeField] private Slider sfxSlider;
+    [SerializeField] private Slider ambientSlider;
 
     private void Start()
     {
-        bgmSlider.onValueChanged.AddListener(SoundManager.Instance.ChangeBGMVolume);
-        sfxSlider.onValueChanged.AddListener(SoundManager.Instance.ChangeSFXVolume);
-
-        // 초기 볼륨 설정
-        bgmSlider.value = SoundManager.Instance.GetBGMVolume();
-        sfxSlider.value = SoundManager.Instance.GetSFXVolume();
+        if (bgmSlider != null)
+        {
+            bgmSlider.onValueChanged.AddListener(SoundManager.Instance.ChangeBGMVolume);
+            bgmSlider.value = SoundManager.Instance.GetBGMVolume();
+        }
+        
+        if (sfxSlider != null)
+        {
+            sfxSlider.onValueChanged.AddListener(SoundManager.Instance.ChangeSFXVolume);
+            sfxSlider.value = SoundManager.Instance.GetSFXVolume();
+        }
+        
+        if (ambientSlider != null)
+        {
+            ambientSlider.onValueChanged.AddListener(SoundManager.Instance.ChangeAmbientVolume);
+            ambientSlider.value = SoundManager.Instance.GetAmbientVolume();
+        }
     }
 }
