059f31ff9f4a23a1efde5b390b3bd8fc25ea8fd7
eunvin <80307318+dmsqls8580@users.noreply.github.com>
2025-08-05 11:25:44 +0900

    fix: 덤불 채집시 스프라이트 변경
    - 덤불 채집시 스프라이트 변경

--- Assets/02_Scripts/World/Resource/UndestroyableObject.cs
+++ Assets/02_Scripts/World/Resource/UndestroyableObject.cs
@@ -1,28 +1,46 @@
 using _02_Scripts.Manager;
 using PlayerStates;
-using System;
 using Unity.VisualScripting;
+using UnityEngine;
+
+public enum BushType
+{
+    BerryBush,  // 베리 덤불: 상호작용 시 베리 드롭 + 스프라이트 변경
+    TwigBush    // 잔가지 덤불: 잔가지 드롭, 스프라이트 변경 없음
+}
 
 public class UndestroyableObject : BaseInteractableObject, IInteractable
 {
+    [Header("Bush Type")]
+    [SerializeField] private BushType bushType;
+
+    [Header("Sprite Settings (for BerryBush only)")]
+    [SerializeField] private SpriteRenderer spriteRenderer;
+    [SerializeField] private Sprite berrylessSprite;
+
     public override void Interact(InteractionCommandType _type, PlayerController _playerController)
     {
-        if(isInteracted)
+        if (isInteracted)
             return;
 
         isInteracted = true;
-        
+
         var toolController = _playerController.GetComponent<ToolController>();
-        float toolPower = toolController.IsUnityNull() ? 1f : toolController.GetAttackPow();
+        float toolPower = toolController == null ? 1f : toolController.GetAttackPow();
 
         SoundManager.Instance.PlaySFX(SFX.ToolHand);
-        
+
         TakeInteraction(toolPower);
 
         if (currentHealth <= 0)
         {
             DropItems(_playerController.transform);
+
+            // 베리 덤불일 경우 스프라이트 변경
+            if (bushType == BushType.BerryBush && spriteRenderer != null && berrylessSprite != null)
+            {
+                spriteRenderer.sprite = berrylessSprite;
+            }
         }
     }
 }
-
